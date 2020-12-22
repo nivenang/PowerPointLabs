76b1f110789e5f06e05efb347dbc45004d54eaad
nivenang <46707122+nivenang@users.noreply.github.com>
2020-12-22 15:53:16 +0800

    Update C++ Syntax Highlighter to match Visual Studio IDE colour scheme

--- PowerPointLabs/PowerPointLabs/LiveCodingLab/Lexer/Grammars/CppGrammar.cs
+++ PowerPointLabs/PowerPointLabs/LiveCodingLab/Lexer/Grammars/CppGrammar.cs
@@ -11,49 +11,50 @@ namespace PowerPointLabs.LiveCodingLab.Lexer.Grammars
             Rules = new List<LexicalRule>()
             {
                 new LexicalRule { Type = TokenType.Comment, RegExpression = new Regex("^(//.*)") }, // Comment
-                new LexicalRule { Type = TokenType.Comment, RegExpression = LexicalRule.WordRegex("#include", "#define") },
                 new LexicalRule { Type = TokenType.Comment, RegExpression = new Regex(@"/\*(?:(?!\*/).)*\*/") },
                 new LexicalRule { Type = TokenType.Comment, RegExpression = new Regex(@"/\*(?:(?!\*/).)*") },
                 new LexicalRule { Type = TokenType.Comment, RegExpression = new Regex(@"(?:(?!\*/).)*\*/") },
-                new LexicalRule { Type = TokenType.Comment, RegExpression = new Regex("^(\\*.*)") },
+                //new LexicalRule { Type = TokenType.Comment, RegExpression = new Regex(@"^\*.*") },
+                new LexicalRule { Type = TokenType.Metadata, RegExpression = LexicalRule.WordRegex("#include", "#define") },
                 new LexicalRule { Type = TokenType.WhiteSpace, RegExpression = new Regex("^\\s") }, // Whitespace
 
-                new LexicalRule 
-                { 
-                    Type = TokenType.Keyword, 
+                new LexicalRule
+                {
+                    Type = TokenType.Keyword,
                     RegExpression = LexicalRule.WordRegex(
                         "asm", "auto", "bool", "break", "case", "catch", "char", "class", "const", "const_cast", "continue",
                         "default", "delete", "do", "double", "dynamic_cast", "else", "enum", "explicit", "export", "extern", "false",
                         "float", "for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace", "new", "NULL",
-                        "operator", "private", "protected", "public", "register", "reinterpret_cast", "return", "short", "signed", 
-                        "sizeof", "size_t", "static", "static_cast", "struct", "switch", "template", "this", "throw", "true", "try", "typedef", 
-                        "typeid", "typename", "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while") 
+                        "operator", "private", "protected", "public", "register", "reinterpret_cast", "return", "short", "signed",
+                        "sizeof", "size_t", "static", "static_cast", "struct", "switch", "template", "this", "throw", "true", "try", "typedef",
+                        "typeid", "typename", "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while")
                 }, // Keywords
                 
                 new LexicalRule { Type = TokenType.Identifier, RegExpression = new Regex("^[_A-Za-z][_A-Za-z0-9]*") }, // Identifier
                 new LexicalRule { Type = TokenType.String, RegExpression = new Regex("^((@'(?:[^']|'')*'|'(?:\\.|[^\\']|)*('|\\b))|(@\"(?:[^\"]|\"\")*\"|\"(?:\\.|[^\\\"])*(\"|\\b)))", RegexOptions.IgnoreCase) }, // String Marker
-                new LexicalRule { Type = TokenType.String, RegExpression = new Regex("(<.*>)") },
+                // new LexicalRule { Type = TokenType.String, RegExpression = new Regex("(<.*>)") },
                 new LexicalRule { Type = TokenType.Unknown, RegExpression = new Regex("^.") }, // Any
             };
 
             ColorDict = new Dictionary<TokenType, Color>()
             {
-                { TokenType.Comment, Color.FromArgb(255, 40, 200, 0) },
-                { TokenType.String, Color.FromArgb(255, 0, 0, 171) },
-                { TokenType.Builtins, Color.FromArgb(255, 144, 0, 144) },
-                { TokenType.Keyword, Color.FromArgb(255, 255, 119, 0) },
+                { TokenType.Comment, Color.FromArgb(128, 0, 128, 0) },
+                { TokenType.String, Color.FromArgb(128, 21, 21, 163) },
+                { TokenType.Builtins, Color.FromArgb(128, 128, 0, 128) },
+                { TokenType.Keyword, Color.FromArgb(128, 255, 0, 0) },
                 { TokenType.Identifier, Color.FromArgb(0, 0, 0, 0) },
                 { TokenType.Delimiter, Color.FromArgb(0, 0, 0, 0) },
                 { TokenType.WhiteSpace, Color.FromArgb(0, 0, 0, 0) },
                 { TokenType.Number, Color.FromArgb(0, 0, 0, 0) },
                 { TokenType.Operator, Color.FromArgb(255, 0, 0, 0) },
+                { TokenType.Metadata, Color.FromArgb(128, 128, 128, 128) },
                 { TokenType.Unknown, Color.FromArgb(0, 0, 0, 0) },
             };
         }
 
         public string Name
         {
-            get { return "Java"; }
+            get { return "Cpp"; }
         }
 
         public List<LexicalRule> Rules { get; private set; }
